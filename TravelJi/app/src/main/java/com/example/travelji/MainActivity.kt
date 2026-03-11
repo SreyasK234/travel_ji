6d35594d921d4cb78b765189fafaf1579d01e8fd
Sreyas K <sreyas234@gmail.com>
2026-03-11 14:44:48 +0530

    hi

--- TravelJi/app/src/main/java/com/example/travelji/MainActivity.kt
+++ TravelJi/app/src/main/java/com/example/travelji/MainActivity.kt
@@ -14,6 +14,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.travelji.ui.theme.TravelJiTheme
 
 class MainActivity : ComponentActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
